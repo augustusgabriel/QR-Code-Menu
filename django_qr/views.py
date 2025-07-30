7a358a346802a236b5d0e33aad02fe8cf2245209
Gabriel Augusto <Avgvstvs_G@protonmail.com>
2025-07-29 22:44:25 -0300

    ADD: Media Files, Logic that generates QR Code && Page to download QR Code

--- django_qr/views.py
+++ django_qr/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render
 from .forms import QrCodeForm
 import qrcode
+import os
+from django.conf import settings
 
 
 def generate_qr_code(request):
-    if request.method == 'post':
+    if request.method == 'POST':
         form = QrCodeForm(request.POST)
         if form.is_valid():
-            res_name = form.cleaned_data('restaurant_name')
-            url = form.cleaned_data('url')
+            res_name = form.cleaned_data['restaurant_name']
+            url = form.cleaned_data['url']
 
             # Generating QR Code
             qr = qrcode.make(url)
-            print(qr)
+            filename = res_name.replace(" ", "_").lower() + "_menu.png"
+            filepath = os.path.join(settings.MEDIA_ROOT, filename)
+            qr.save(filepath)
+
+            # Image URL
+            qr_url = os.path.join(settings.MEDIA_URL, filename)
+
+            context = {
+                'res_name': res_name,
+                'qr_url': qr_url,
+                'filename': filename,
+            }
+
+            return render(request, 'qr_result.html', context)
     else:
         form = QrCodeForm()
         context = {
